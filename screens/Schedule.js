e07a19a1d1f86e90717fc7800452efeae065b4eb
Isaac Ivins <isaacivins@isaacs-air.attlocal.net>
2017-06-28 14:12:26 -0400

    added nav buttons with state change

--- screens/Schedule.js
+++ screens/Schedule.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import ToggleButton from '../components/toggleButton';
 
 export default class Schedule extends React.Component {
+
+  state = {
+    selectedDay: 'THURSDAY',
+  };
+
+  handlePressedItem = (item) => {
+    this.setState({
+      selectedDay: item
+    })
+  }
+
   render() {
+
+    const { selectedDay } = this.state;
+
     return (
       <View style={styles.container}>
-        <Image source={require("../assets/icons/avengers.png")}/>
+        <Image style={styles.images} source={require("../assets/icons/avengers.png")}>
+          <Image style={styles.logo} source={require("../assets/icons/app.png")} />
+          <Text style={styles.title}> Avengers Squad </Text>
+          <ToggleButton
+            items={['THURSDAY', 'FRIDAY']}
+            value={selectedDay}
+            onPressItem={this.handlePressedItem}
+          />
+        </Image>
       </View>
     );
   }
@@ -17,7 +40,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff'
   },
   images: {
-    height: 200,
-    width: null
+    height: 180,
+    width: null,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  logo: {
+    width: 60,
+    height: 60,
+    marginBottom: 10
+  },
+  title: {
+    backgroundColor: 'transparent',
+    color: 'white',
+    fontSize: 24
   }
 });
